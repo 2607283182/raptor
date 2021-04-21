fbe83e1f05b8050149a776a67d812f752fb9631c
登峰造极境 <39618622+22297749@users.noreply.github.com>
2021-04-21 11:24:39 +0800

    整改配置

--- raptor-login-service/src/main/java/com/caosong/raptor/login/controller/TbUserController.java
+++ raptor-login-service/src/main/java/com/caosong/raptor/login/controller/TbUserController.java
@@ -1,8 +1,12 @@
 package com.caosong.raptor.login.controller;
 
 
+import com.caosong.raptor.login.service.ITbUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,5 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login/tb-user")
 public class TbUserController {
+    @Autowired
+    private ITbUserService service;
+    @RequestMapping("/test")
+    public List test(){
+        return service.list();
+    }
 
 }
